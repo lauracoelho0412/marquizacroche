7b028b119b73faa15e80eab0b9a7c4be9fb678c3
Laura Coelho <147345356+lauracoelho0412@users.noreply.github.com>
2025-03-21 16:31:23 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
-//esperar a pagina ser carregada//
-document.addEventListener("DOMContentLoaded",function(){
-    function myFunction() {
-        var x = document.getElementById("navbar");
-        if (x.className === "topnav") {
-          x.className += " responsive";
-        } else {
-          x.className = "topnav";
-        }
-      }
-})
+function myFunction() {
+  var x = document.getElementById("navbar");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
