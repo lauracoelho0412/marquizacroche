7a1b048526dc0675d427fe23e28d1a80b146cc5e
Laura Coelho <147345356+lauracoelho0412@users.noreply.github.com>
2025-04-08 18:13:09 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -10,28 +10,4 @@ document.addEventListener("DOMContentLoaded", (event) => {
       x.className = "topnav";
     }
   }
-
-  const buttonsWrapper = document.querySelector(".map");
-  const slides = document.querySelector(".inner");
-
-  buttonsWrapper.addEventListener("click", e => {
-    if (e.target.nodeName === "BUTTON") {
-      Array.from(buttonsWrapper.children).forEach(item =>
-        item.classList.remove("active")
-      );
-      if (e.target.classList.contains("first")) {
-        slides.style.transform = "translateX(-0%)";
-        e.target.classList.add("active");
-      } else if (e.target.classList.contains("second")) {
-        slides.style.transform = "translateX(-40%)";
-        e.target.classList.add("active");
-      } else if (e.target.classList.contains('third')) {
-        slides.style.transform = 'translatex(-80%)';
-        e.target.classList.add('active');
-      }
-    }
-  });
-
-  
-
 })
